42c6bc8fc67be91472a8ce2a20952c47bf3e854c
mangi07 <mangi07@users.noreply.github.com>
2016-05-25 20:29:41 +0100

    Started setting up data between server and client.

--- hangman.rb
+++ hangman.rb
@@ -4,30 +4,37 @@ also_reload 'ben.rb'
 
 require_relative 'ben'
 
-
-
+# starting with fresh settings
+set :styles, {:background => "#000", :color => "#fff"}
+set :game_data, nil
 
 get '/' do
-	message = ""
-	if params["guess"] && params["guess"] != ""
-		guess = Integer( params["guess"] )
-		message = check_guess( guess )
-		params["guess"] = ""
-	end
-	if params["cheat"] && params["cheat"] == "true"
-		message << "<p>#{settings.number}</p>"
-	end
-	message << "<p>You have #{Ben::Counter.get} guesses remaining</p>"
 	erb :index, :locals => {
 		:styles => settings.styles,
-		:message => message
+		:game_data => settings.game_data
 	}
-	#throw params.inspect
 end
 
+# get '/guess' do
+# 	difficulty
+# 	guesses
+
+# 	if params["guess"] && params["guess"] != ""
+# 		guess = params["guess"]
+# 		params["guess"] = ""
+# 	end
+# 	if params["cheat"] && params["cheat"] == "true"
+# 		message << "<p>#{settings.number}</p>"
+# 	end
+# 	message << "<p>You have #{Ben::Counter.get} guesses remaining</p>"
+# 	erb :index, :locals => {
+# 		:styles => settings.styles,
+# 		:message => message
+# 	}
+# 	#throw params.inspect
+# end
+
 # TODO
-#
-# rename this file to hangman.rb
 #
 # Use routes like '/game/load' and '/game/save' and '/game/init'
 # with jquery ajax
